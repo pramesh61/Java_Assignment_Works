5c06eafb95a32cadd05e42667ae7b385acc18dc2
Pramesh Lamichhane <103353707+pramesh61@users.noreply.github.com>
2025-12-10 22:30:11 +0545

    Remove comments from accelerate and brake methods
    Removed comments describing hidden complex logic in methods.

--- objectorientedprinciples/abstraction/Bike.java
+++ objectorientedprinciples/abstraction/Bike.java
@@ -4,12 +4,12 @@ public class Bike extends Vehicle {
     @Override
     void accelerate() {
         System.out.println("Bike: Accelerating accelerator...");
-        // Hidden complex logic: fuel injection, gear shifting, etc.
+
     }
 
     @Override
     void brake() {
         System.out.println("Bike: Applying brakes...");
-        // Hidden logic: hydraulic pressure, brake pads, etc.
+
     }
 }
